94ffc6ca46377aac01f82c74e3f45b3e626a781e
egesenli <egesenli@gmail.com>
2023-04-13 18:50:43 -0400

    Initialize the Game with two players

---
+++ lib/game.rb
@@ -0,0 +1,8 @@
+class Game
+  def initialize
+    @player1 = Player.new("Player 1")
+    @player2 = Player.new("Player 2")
+    @scoreboard = Scoreboard.new(@player1, @player2)
+    @current_player = @player1
+  end
+end
