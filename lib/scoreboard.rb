f68493fa9d2507884844678c1c01b9853d303d60
egesenli <egesenli@gmail.com>
2023-04-13 19:29:12 -0400

    Refactor scoreboard class and optimize the methods

--- lib/scoreboard.rb
+++ lib/scoreboard.rb
@@ -1,28 +1,18 @@
 class Scoreboard
-  def initialize(player1, player2)
-    @player1 = player1
-    @player2 = player2
+  def initialize(players)
+    @players = players
   end
 
-  def display_score
-    puts "Scoreboard:"
-    puts "#{@player1.name}: #{@player1_score} points, #{@player1_lives} lives"
-    puts "#{@player2.name}: #{@player2_score} points, #{@player2_lives} lives"
+  def to_s
+    scores = @players.map { |player| "#{player.name}: #{player.score} points, #{player.lives} lives" }
+    scores.join(" vs ")
   end
 
   def update_score(player)
-    if player == @player1
-      @player1_score += 1
-    else
-      @player2_score += 1
-    end
+    player.add_point
   end
 
   def update_lives(player)
-    if player == @player1
-      @player1_lives = player.lives
-    else
-      @player2_lives = player.lives
-    end
+    # Update lives
   end
 end
