cd914d297188ea2996696864a79886760991c1c4
egesenli <egesenli@gmail.com>
2023-04-13 18:46:04 -0400

    Implement to_s method to display scores

--- lib/scoreboard.rb
+++ lib/scoreboard.rb
@@ -2,4 +2,9 @@ class Scoreboard
   def initialize(players)
     @players = players
   end
+
+  def to_s
+    scores = @players.map { |player| "#{player.name}: #{player.score}" }
+    scores.join(" vs ")
+  end
 end
