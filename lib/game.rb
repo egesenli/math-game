8f4326372dc3e04a3034643722199d78539f2135
egesenli <egesenli@gmail.com>
2023-04-13 19:29:43 -0400

    Update play method according to new scoreboard class

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,11 @@
 require_relative 'player'
 require_relative 'question'
 require_relative 'scoreboard'
-
 class Game
   def initialize
     @player1 = Player.new("Player 1")
     @player2 = Player.new("Player 2")
-    @scoreboard = Scoreboard.new(@player1, @player2)
+    @scoreboard = Scoreboard.new([@player1, @player2])
     @current_player = @player1
   end
 
@@ -16,7 +15,7 @@ class Game
       @current_player.answer_question(question)
 
       @scoreboard.update_lives(@current_player)
-      @scoreboard.display_score
+      puts @scoreboard.to_s
 
       break if game_over?
 
