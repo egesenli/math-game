db04fd0f4f15c50e7c3123f56f66c283ed0f9af5
egesenli <egesenli@gmail.com>
2023-04-13 18:51:09 -0400

    Implement the game loop, calling Player#answer_question and updating the Scoreboard

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,14 @@
 class Game
-  def initialize
-    @player1 = Player.new("Player 1")
-    @player2 = Player.new("Player 2")
-    @scoreboard = Scoreboard.new(@player1, @player2)
-    @current_player = @player1
+  def play
+    loop do
+      question = Question.new
+      @current_player.answer_question(question)
+      @scoreboard.update_lives(@current_player)
+      @scoreboard.display_score
+
+      break if game_over?
+
+      switch_player
+    end
   end
 end
